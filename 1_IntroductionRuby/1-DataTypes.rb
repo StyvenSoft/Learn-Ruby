6bfef4705c330d2632ce41113d4ba411b099e1f4
Steveen <steveen.echeverri@gmail.com>
2020-11-22 11:59:50 -0500

    Introduction Ruby

---
+++ 1_IntroductionRuby/1-DataTypes.rb
@@ -0,0 +1,24 @@
+puts "Hello World!" # Puts Strings
+
+print "This text will bump right into" # Print Strings
+
+my_num =  25   # Numbers!
+
+my_num = 100 # variables
+
+my_boolean = true    # Booleans!
+
+my_string = "Ruby"  # Strings.
+
+my_array = [ "Apple", "Hi", 3.142, true, 56, ] # Arrays
+
+Fruits_hash = { "Apple" => 10, "Banana" => 20, "Kiwi" => 30 } # Hashes
+
+my_symbols = {:ap => "Apple", :bn => "Banana", :mg => "Mango"} # Symbols
+
+# Maths
+
+2**3 
+sum = 13 + 379
+product = 923 * 15
+quotient = 13209 / 17
---
+++ 1_IntroductionRuby/2-Methods.rb
@@ -0,0 +1,37 @@
+=begin
+I'm a comment!
+I don't need any # symbols.
+=end
+
+puts "I love espresso".length
+# 15 
+
+puts "Jamie".length
+
+# 5
+
+puts "Eric".reverse
+
+# "cirE"
+
+puts "Jamie".reverse
+
+# "eimaJ"
+
+puts "eric".upcase    
+
+# ERIC
+
+puts "Jamie".upcase
+
+# JAMIE
+
+puts "Jamie".downcase
+
+# jamie
+
+name = "Steveen"
+
+puts name.downcase.reverse.upcase
+
+# NEEVETS
